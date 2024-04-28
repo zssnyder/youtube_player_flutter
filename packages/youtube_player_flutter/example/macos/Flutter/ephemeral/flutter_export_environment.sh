7c1b50d03f30378fdf06337faa176d34842dc689
zssnyder <zack.s.snyder@gmail.com>
2024-04-28 14:12:05 -0700

    ~ Pulled from master

---
+++ packages/youtube_player_flutter/example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/zssnyder/Projects/Libraries/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/zssnyder/Projects/Portal/app/portal_app/packages/youtube_player_flutter/packages/youtube_player_flutter/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=7.0.0"
+export "FLUTTER_BUILD_NUMBER=7.0.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
