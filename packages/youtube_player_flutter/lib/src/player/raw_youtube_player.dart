7d2ad055b9fc2986b01ea8d0c7dfaf26d93c17a0
zssnyder <zack.s.snyder@gmail.com>
2025-09-18 12:56:41 -0700

    ~ Reverting user agent changes

--- packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
+++ packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
@@ -382,5 +382,5 @@ class _RawYoutubePlayerState extends State<RawYoutubePlayer> with WidgetsBinding
 
   String get userAgent => controller!.flags.forceHD
       ? 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'
-      : 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1';
+      : '';
 }
